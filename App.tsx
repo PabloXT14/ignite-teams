54fca4e183efd57060638bba6688f3bf070d8022
Pablo Alan <pabloxt14@gmail.com>
2024-10-25 19:51:05 -0300

    style: defining app theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Groups } from '@/screens/Groups'
+import { defaultTheme } from '@/theme'
 import { StatusBar } from 'expo-status-bar'
+import { ThemeProvider } from 'styled-components'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
       <Groups />
-    </>
+    </ThemeProvider>
   )
 }
