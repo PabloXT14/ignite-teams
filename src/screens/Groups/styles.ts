b59469724b3316f3d54fd33a5d7036ab297543df
Pablo Alan <pabloxt14@gmail.com>
2024-10-26 20:37:20 -0300

    feat: create header component

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,12 +3,4 @@ import { styled } from 'styled-components/native'
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.gray[600]};
-  align-items: center;
-  justify-content: center;
-`
-
-export const Title = styled.Text`
-  color: #fafafa;
-  font-size: 24px;
-  font-weight: bold;
 `
