2691233f43b472b8b20cfa26199c1bf1e54b8f09
Pablo Alan <pabloxt14@gmail.com>
2024-10-26 00:42:13 -0300

    style: custom fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,23 @@ import { Groups } from '@/screens/Groups'
 import { defaultTheme } from '@/theme'
 import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
