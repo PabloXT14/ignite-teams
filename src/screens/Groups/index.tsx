c4950a9f50486800d3e47e09cbab3091e0b489be
Pablo Alan <pabloxt14@gmail.com>
2024-11-03 16:55:03 -0300

    feat: implementing go back

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,10 +5,10 @@ import { useNavigation } from '@react-navigation/native'
 import { Header } from '@/components/Header'
 import { Highlight } from '@/components/Highlight'
 import { GroupCard } from '@/components/GroupCard'
-
-import { Container } from './styles'
 import { ListEmpty } from '@/components/ListEmpty'
 import { Button } from '@/components/Button'
+
+import { Container } from './styles'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,11 +1,19 @@
+import { useNavigation } from '@react-navigation/native'
+
 import { Header } from '@/components/Header'
 import { Highlight } from '@/components/Highlight'
 import { Button } from '@/components/Button'
-
-import { Container, Content, Icon } from './styles'
 import { Input } from '@/components/Input'
 
+import { Container, Content, Icon } from './styles'
+
 export function NewGroup() {
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,7 +28,11 @@ export function NewGroup() {
 
         <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNewGroup}
+        />
       </Content>
     </Container>
   )
