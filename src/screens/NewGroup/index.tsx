aff757ad71d06ae9552c51c636421fe3fe00293b
Pablo Alan <pabloxt14@gmail.com>
2024-11-05 20:44:19 -0300

    feat: handle exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '@/components/Header'
@@ -6,6 +7,7 @@ import { Highlight } from '@/components/Highlight'
 import { Button } from '@/components/Button'
 import { Input } from '@/components/Input'
 
+import { AppError } from '@/utils/AppError'
 import { groupCreate } from '@/storage/group/group-create'
 
 import { Container, Content, Icon } from './styles'
@@ -16,13 +18,21 @@ export function NewGroup() {
   const navigation = useNavigation()
 
   async function handleNewGroup() {
-    if (group.trim().length === 0) return
+    if (group.trim().length === 0) {
+      return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+    }
 
     try {
       await groupCreate(group)
       navigation.navigate('players', { group })
     } catch (error) {
       console.log(error)
+
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar o grupo.')
+      }
     }
   }
 
